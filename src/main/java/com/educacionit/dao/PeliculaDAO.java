8d0204cc9ba81cb0171d3c19a9f6bd14a6abd1bf
Emmanuel Greco <emma.greco.96@gmail.com>
2024-04-12 19:09:47 -0300

    Implemento busqueda-obtener-todas-peliculas

--- src/main/java/com/educacionit/dao/PeliculaDAO.java
+++ src/main/java/com/educacionit/dao/PeliculaDAO.java
@@ -8,7 +8,8 @@ import com.educacionit.dao.model.Pelicula;
 public interface PeliculaDAO {
 	
 	public void buscarPeliculasPorTitulo(String titulo) throws SQLException;
-	List<Pelicula> obtenerPeliculasPorTitulo(String titulo) throws SQLException;	
+	List<Pelicula> obtenerPeliculasPorTitulo(String titulo) throws SQLException;
+	void buscarTodasPeliculas() throws SQLException;
+	List<Pelicula> obtenerTodasPeliculas() throws SQLException;
 	Pelicula mostrarDetallePelicula(int codigo) throws SQLException;
-
 }
